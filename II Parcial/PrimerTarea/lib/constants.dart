d705634afbd585ee5666321c844a757a24585e23
SadanaBalcazar <e1719541193@live.uleam.edu.ec>
2022-12-01 20:50:04 -0500

    Primer tarea de segundo parcial subida con éxito

---
+++ II Parcial/PrimerTarea/lib/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Colors.grey;
+const line = Colors.red;
+
+const fromTextStyle = TextStyle(
+  fontSize: 15.0,
+  fontWeight: FontWeight.w700,
+);
+
+const dateTextStyle = TextStyle(
+  color: Colors.black45,
+  fontSize: 14.0,
+  fontWeight: FontWeight.w300,
+);
+
+const subjectTextStyle = TextStyle(
+  fontSize: 15.0,
+  fontWeight: FontWeight.w400,
+);
+
+const bodyTextStyle = TextStyle(
+  fontSize: 15.0,
+);
